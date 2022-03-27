613b1fde45f7f186f54a8c1fde5be1545c24c9a1
Avitej <82682098+Avitej@users.noreply.github.com>
2022-03-26 20:49:53 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -29,3 +29,48 @@ function speak() {
     speak_data_2 = "the second prediction is "+prediction_2;
     synth.speak(utterThis);
 }
+
+function check()
+{
+  img = document.getElementById('captured_image');
+  classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results) {
+if (error) {
+  console.error(error);
+} else {
+  console.log(results);
+  document.getElementById("result_emotion_name").innerHTML = results[0].label;
+  document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+  prediction_1 = results[0].label;
+  prediction_2 = results[1].label;
+  speak();
+  if(results[0].label == "OK")
+  {
+ document.getElementById("update_emoji").innerHTML = "&#128076;";
+  }
+  if(results[0].label == "Good job")
+  {
+ document.getElementById("update_emoji").innerHTML = "&#128077;";
+  }
+  if(results[0].label == "Peace")
+  {
+ document.getElementById("update_emoji").innerHTML = "&#9996;";
+  }
+
+  if(results[1].label == "OK")
+  {
+ document.getElementById("update_emoji2").innerHTML = "&#128076;";
+  }
+  if(results[1].label == "Good job")
+  {
+ document.getElementById("update_emoji2").innerHTML = "&#128077;";
+  }
+  if(results[1].label == "Peace")
+  {
+ document.getElementById("update_emoji2").innerHTML = "&#9996;";
+  }
+}
+}
